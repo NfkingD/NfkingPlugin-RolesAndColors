b30a8475eca50aa0c1dc6ab3b29ee1570fc35cad
NfkingD <87357690+NfkingD@users.noreply.github.com>
2022-07-02 12:50:54 +0200

    Refactored ArgumentErrorHandling

--- src/main/java/com/nfkingd/nfkingplugin/roles_and_colors/commands/RoleCommand.java
+++ src/main/java/com/nfkingd/nfkingplugin/roles_and_colors/commands/RoleCommand.java
@@ -13,36 +13,49 @@ import java.util.Optional;
 public class RoleCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String alias,  String[] arguments) {
-            if (!commandSender.hasPermission("roles.admin")) {
-                sendErrorMessage(commandSender, "You don't have the permission");
-                return true;
-            }
-
-            if (arguments.length == 0) {
-                sendErrorMessage(commandSender, "This command requires arguments");
-                return true;
-            }
-
-            var firstArgument = arguments[0];
-
-            if (firstArgument.equals("set")) {
-                return processSetCommand(commandSender, arguments);
-            }
-
-            var message = "/role " + arguments[0] + " does not exist";
-            sendErrorMessage(commandSender, message);
-
+    public boolean onCommand(CommandSender commandSender, Command command, String alias, String[] arguments) {
+        if (!commandSender.hasPermission("roles.admin")) {
+            sendErrorMessage(commandSender, "You don't have the permission");
             return true;
         }
+
+        if (arguments.length == 0) {
+            sendErrorMessage(commandSender, "This command requires arguments");
+            return true;
+        }
+
+        var firstArgument = arguments[0];
+
+        if (firstArgument.equals("set")) {
+            return processSetCommand(commandSender, arguments);
+        }
+
+        if (firstArgument.equals("create")) {
+            return processCreateCommand(commandSender, arguments);
+        }
+
+        var message = "/role " + arguments[0] + " does not exist";
+        sendErrorMessage(commandSender, message);
+
+        return true;
+    }
+
+    private boolean processCreateCommand(CommandSender commandSender, String[] arguments) {
+        var argumentsCount = arguments.length;
+
+        if (sendErrorMessageForUnhandledArgumentCount(2, 4, argumentsCount, commandSender)) {
+            return true;
+        }
+
+
+        return true;
+    }
 
     private boolean processSetCommand(CommandSender commandSender, String[] arguments) {
         var argumentsCount = arguments.length;
 
-        if (argumentsCount < 3) {
-            return sendErrorMessageForMissingArguments(argumentsCount, commandSender);
-        } else if (argumentsCount > 5) {
-            return sendErrorMessageForExcessArguments(argumentsCount, commandSender);
+        if (sendErrorMessageForUnhandledArgumentCount(3, 5, argumentsCount, commandSender)) {
+            return true;
         }
 
         var optionalPlayer = getPlayer(commandSender, arguments);
@@ -61,16 +74,27 @@ public class RoleCommand implements CommandExecutor {
         return true;
     }
 
-    private boolean sendErrorMessageForMissingArguments(int argumentsCount, CommandSender commandSender) {
-        var missingArgumentCount = 3 - argumentsCount;
+    private boolean sendErrorMessageForUnhandledArgumentCount(int minimalArgumentCount, int maximalArgumentCount,
+                                                              int argumentsCount, CommandSender commandSender) {
+        if (argumentsCount < minimalArgumentCount) {
+            return sendErrorMessageForMissingArguments(minimalArgumentCount, argumentsCount, commandSender);
+        } else if (argumentsCount > maximalArgumentCount) {
+            return sendErrorMessageForExcessArguments(maximalArgumentCount, argumentsCount, commandSender);
+        }
+
+        return false;
+    }
+
+    private boolean sendErrorMessageForMissingArguments(int minimalArgumentCount, int argumentsCount, CommandSender commandSender) {
+        var missingArgumentCount = minimalArgumentCount - argumentsCount;
         var message = "You must add at least " + missingArgumentCount + " more argument(s)";
         sendErrorMessage(commandSender, message);
 
         return true;
     }
 
-    private boolean sendErrorMessageForExcessArguments(int argumentsCount, CommandSender commandSender) {
-        var excessArgumentCount = argumentsCount - 5;
+    private boolean sendErrorMessageForExcessArguments(int maximalArgumentCount, int argumentsCount, CommandSender commandSender) {
+        var excessArgumentCount = argumentsCount - maximalArgumentCount;
         var message = "You must remove at least " + excessArgumentCount + " argument(s)";
         sendErrorMessage(commandSender, message);
 
